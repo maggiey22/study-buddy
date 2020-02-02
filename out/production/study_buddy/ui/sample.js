3d70004d9d2bb2b3d13b3e7c43dfc7b1d836f7fd
Maggie Yang <43491401+maggiey22@users.noreply.github.com>
2020-02-01 21:27:56 -0800

    upload files part 2

---
+++ out/production/study_buddy/ui/sample.js
@@ -0,0 +1,14 @@
+function msg() {
+    print("Hello World from msg()");
+    // console.log("Hello from sample.js .");
+    //var name = "String";
+    // myMap();
+}
+
+//function myMap() {
+//    var mapProp= {
+//      center:new google.maps.LatLng(51.508742,-0.120850),
+//      zoom:5,
+//    };
+//    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+//}
